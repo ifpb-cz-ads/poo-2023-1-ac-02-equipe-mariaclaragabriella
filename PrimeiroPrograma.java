4d50e87c8bfa30c1639478ddae59f3dbde1ce5d1
Maria Clara Ramalho Medeiros <110572765+marysclair@users.noreply.github.com>
2023-03-22 17:10:33 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+
+/**
+ * Escreva uma descrição da classe PrimeiroPrograma aqui.
+ * 
+ * @author Maria Clara 
+ * @version 22/03/2023
+ */
+public class PrimeiroPrograma
+{
+    public static void start(String[] args){
+        System.out.print("Terminei a primeira aula com um programa Java!");
+    }
+}
